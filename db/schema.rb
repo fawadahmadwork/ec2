83915ba22a327f46e3b3a1c4889fc972ab3f7649
“”fawadahmadwork <fawadahmad99h@gmail.comssh-keygen -t rsa -C fawadahmad99h@gmail.com>
2023-11-02 13:04:27 +0500

    adding books

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_11_02_073929) do
+ActiveRecord::Schema[7.0].define(version: 2023_11_02_080357) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "books", force: :cascade do |t|
+    t.string "name"
+    t.integer "price"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "contacts", force: :cascade do |t|
     t.string "name"
